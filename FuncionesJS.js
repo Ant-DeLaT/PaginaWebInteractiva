bea52470cff0d36d43ffb958f9577c0c536b9d0f
Ant-DeLaT <186017554+Ant-DeLaT@users.noreply.github.com>
2025-01-13 10:55:48 +0100

    cambio3

--- FuncionesJS.js
+++ FuncionesJS.js
@@ -17,6 +17,7 @@ function ponerOscuro(){
 }
 
 function cuantosclicks(){
+    console.log("Cuantos?")
     let clicks;
     (localStorage.getItem("clicks"))?
         (
@@ -28,17 +29,31 @@ function cuantosclicks(){
     }
 
 function hazClick(){
-    let clicks=(localStorage.getItem("clicks")||0);
+    console.log("CLICK!")
+    let clicks;
+    (localStorage.getItem("clicks"))?
+    (clicks=localStorage.getItem("clicks")):
+    (clicks=0);
     clicks++;
     localStorage.setItem("clicks",clicks);
     cuantosclicks;
 }
 
 function cambiaColor(){
+    const setBg = () => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+        color.innerHTML = "#" + randomColor;
+      }
+      
+      genNew.addEventListener("click", setBg);
+      setBg();
 
+
+    console.log("CambiaColor")
     function numAlAzar(){
         return (Math.random()*255+1)
     }
-
-    $("body").css("background-color","rgb ("+numAlAzar+","+numAlAzar+","+numAlAzar+")")
+// rgb ("+numAlAzar+","+numAlAzar+","+numAlAzar+")
+    $("body").css("background-color","random")
 }
