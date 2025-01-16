75df9f48d2f27a2b5a932e98df2bf85652011adf
Ant-DeLaT <186017554+Ant-DeLaT@users.noreply.github.com>
2025-01-16 09:44:10 +0100

    addedAttemptOfScrollBar

--- FuncionesJS.js
+++ FuncionesJS.js
@@ -1,10 +1,42 @@
+function hazClick(){
+    console.log("CLICK!")
+    let clicks;
+    (localStorage.getItem("clicks"))?
+    (clicks=parseInt(localStorage.getItem("clicks"))):
+    (clicks=0);
+    console.log(clicks)
+    clicks=(parseInt(clicks)+1);
+    localStorage.setItem("clicks",clicks);
+    cuantosclicks();
+}
+function reinicioClicks(){
+    console.log("Reiniciado")
+    localStorage.setItem("clicks",0)
+    cuantosclicks()
+}
 
-console.log(!false);
+function cambiaColor(){
+    console.log("CambiaColor")
+    function numAlAzar(){
+        let valor=Math.floor(Math.random()*256);
+        return valor;
+    }
+    let color="rgb("+numAlAzar()+","+numAlAzar()+","+numAlAzar()+")";
+    console.log("VALOR RGB: "+color);
+
+    $("#fondoVoluble").css("background-color",color);
+}
+function cambioBrillo(){
+    let claro=cambioClaro();
+    if(claro==true){ponerOscuro();claro=false}else{ponerClaro();claro=true}
+    console.log("ANtclaro; "+localStorage.getItem("claro"))
+    localStorage.setItem("claro",claro);
+    console.log("dESpclaro; "+localStorage.getItem("claro"))
+}
 function cambioClaro(){
-    let claro=localStorage.getItem("claro");
-    if(claro){ponerOscuro()}else{ponerClaro()};
-    localStorage.setItem("claro",!claro);
-    console.log("claro; "+localStorage.getItem("claro"))
+    let esBlanco=localStorage.getItem("claro");
+    console.log("esBlanco",esBlanco)
+    return esBlanco;
 }
 function ponerOscuro(){
     console.log("PONEROSCURO");
@@ -20,35 +52,53 @@ function ponerClaro(){
 function cuantosclicks(){
     console.log("Cuantos?");
     clicks=localStorage.getItem("clicks");
-    
-    $("#textoClicks").innerText=(""+clicks)
+    $("#textoClicks").text((clicks));
     }
 
-function hazClick(){
-    console.log("CLICK!")
-    let clicks;
-    (localStorage.getItem("clicks"))?
-    (clicks=parseInt(localStorage.getItem("clicks"))):
-    (clicks=0);
-    console.log(clicks)
-    clicks=(parseInt(clicks)+1);
-    localStorage.setItem("clicks",clicks);
-    cuantosclicks();
-}
-function reinicioClicks(){
-    console.log("Reiniciado")
-    localStorage.setItem("clicks",0)
-}
 
-function cambiaColor(){
-    console.log("CambiaColor")
-    function numAlAzar(){
-        let valor=Math.floor(Math.random()*256);
-        return valor;
+const barraProgreso=$("#barraProgreso");
+const seccion= $("seccion");
+const progresoBarra=function(){
+    let cantidadBajada=-seccion.getBoundingClientRect().top;
+    let porcentajeProgreso=(cantidadBajada/(Selection.getBoundingClientRect().height-document.documentElement.clientHeight))*100;
+    let val=Math.floor(porcentajeProgreso);
+
+    (val<0)?(val=0):null;
+    barraProgreso.style.width=val+"%";
+}
+window.addEventListener('scroll',progresoBarra)
+// JAVASCRIPT LISTO
+$(document).ready(()=>{
+    function textoSaludo(){
+        let hora=Math.floor((Date.now()/(3600*1000)/24+1)%24)
+        if (hora<12) {
+            buenosDias()
+        } else if(hora<20) {
+            buenasTardes()
+        }else{
+            buenasNoches()
+        }
     }
-    let color="rgb("+numAlAzar()+","+numAlAzar()+","+numAlAzar()+")";
-    console.log("VALOR RGB: "+color);
+    function buenosDias(){
+        $("#paraCambiar").text("Buenos días");
+    }
+    function buenasTardes(){
+        $("#paraCambiar").text("Buenas tardes");
+    }
+    function buenasNoches(){
+        $("#paraCambiar").text("Que descanseis milord/milady");
+    }
 
-    $("body").css("background-color",color);
-    
+
+    const detectado =new SpeechRecognition();
+    const listaReconocida = new SpeechGrammarList();
+
+    function parlanchin(){
+        
+    }
+
+
+    textoSaludo()
 }
+
+)
